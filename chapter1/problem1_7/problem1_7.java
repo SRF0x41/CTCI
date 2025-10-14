b48623e3b998b132172abb006691e6db79ddba8e
Sergio Rodriguez <sergiorodriguez@MacBookPro.attlocal.net>
2025-10-14 17:47:57 -0600

    finished problem 7

--- chapter1/problem1_7/problem1_7.java
+++ chapter1/problem1_7/problem1_7.java
@@ -7,38 +7,43 @@ public class problem1_7 {
         /*Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4
         bytes, write a method to rotate the image by 90 degrees. Can you do this in place? */
 
+        int[][] m = generateMatrix();
+        System.out.println("Before");
+        printMatrix(m);
+        System.out.println("After Brute Force Method");
+        printMatrix(problem1_7_BF(m));
+
     }
 
+    public static int[][] problem1_7_BF(int[][] m){
+        int rows = m.length;
+        int cols = m[0].length;
+        int[][] transposed_m = new int[rows][cols];
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                transposed_m[c][rows - r - 1] = m[r][c];
+            }
+        }
+        return transposed_m;
+    }
 
     public static int[][] generateMatrix() {
-        int min = 3; // Minimum value (inclusive)
-        int max = 3; // Maximum value (inclusive)
-        int randRow = (int) (Math.random() * (max - min + 1) + min);
-        int randCol = (int) (Math.random() * (max - min + 1) + min);
-        int[][] m = new int[randRow][randCol];
-        for(int row = 0; row < randRow; row++){
-            for(int col = 0; col < randCol; col++){
-                int rand = (int) (Math.random() * 10);
-                m[row][col] = rand;
+        int n = 3;
+        int[][] m = new int[n][n];
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                m[r][c] = (int) (Math.random() * 10);
             }
         }
         return m;
     }
-    public static void problem1_7_BF(byte[][] matrix){
-
-    }
 
     public static void printMatrix(int[][] m){
-        String RED = "\u001B[31m";
-        String RESET = "\u001B[0m"; 
         int rows = m.length;
         int cols = m[0].length;
-
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
-               
                     System.out.print(m[row][col]);
-                
             }
             System.out.println("");
         }
